58c0ef6981afe7f633ab7fd2f4af8692580ad49a
Junaid <jahmad095@gmail.com>
2019-06-26 17:52:44 -0400

    recurrsion

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 function sumItems(array) {
   // Sum all the numbers in the array
+  return array.reduce((sum, currentValue) => {
+    if (Array.isArray(currentValue)){
+       return sum  + sumItems(currentValue);
+    } else {
+      return sum + currentValue;
+    }
+  }, 0);
 }
 
 module.exports = sumItems;
